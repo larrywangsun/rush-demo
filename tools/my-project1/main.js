d1334c44d6d883e15915ebe4443004fa61f922ab
larry <larrywangsun@qq.com>
2023-07-30 17:02:49 +0800

    feat: add my-project1

---
+++ tools/my-project1/main.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.subtract = void 0;
+const subtract = (a, b) => {
+    return a - b;
+};
+exports.subtract = subtract;
