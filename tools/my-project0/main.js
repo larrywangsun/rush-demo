cfbd4b5cf01f8c794beb97c3318de2612e3243e9
larry <larrywangsun@qq.com>
2023-07-30 16:53:26 +0800

    feat: add my-project0

---
+++ tools/my-project0/main.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.add0 = void 0;
+const add0 = (a, b) => {
+    return a + b;
+};
+exports.add0 = add0;
---
+++ tools/my-project0/main.ts
@@ -0,0 +1,3 @@
+export const add0 = (a: number, b: number): number => {
+  return a + b;
+};
